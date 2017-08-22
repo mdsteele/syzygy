94e6091eff0f3a984094c597f937142bf65d0f45
Matthew D. Steele <mdsteele@alum.mit.edu>
2017-08-22 17:00:50 -0400

    Add unit tests for "System Failure" puzzle

--- src/save/puzzles/failure.rs
+++ src/save/puzzles/failure.rs
@@ -40,6 +40,7 @@ pub struct FailureState {
 impl FailureState {
     pub fn solve(&mut self) {
         self.access = Access::Solved;
+        self.mid_scene_done = true;
         for coords in Coords::all() {
             if self.board.piece_at(coords).is_none() {
                 let team = if self.board.srb_supply() > 0 {
@@ -87,13 +88,66 @@ impl Tomlable for FailureState {
 
     fn from_toml(value: toml::Value) -> FailureState {
         let mut table = to_table(value);
-        FailureState {
-            access: Access::pop_from_table(&mut table, ACCESS_KEY),
-            mid_scene_done: table.get(MID_SCENE_DONE_KEY)
-                                 .and_then(toml::Value::as_bool)
-                                 .unwrap_or(false),
+        let access = Access::pop_from_table(&mut table, ACCESS_KEY);
+        let mut state = FailureState {
+            access: access,
+            mid_scene_done: bool::pop_from_table(&mut table,
+                                                 MID_SCENE_DONE_KEY),
             board: Board::pop_from_table(&mut table, BOARD_KEY),
+        };
+        if access.is_solved() {
+            state.solve();
         }
+        state
+    }
+}
+
+// ========================================================================= //
+
+#[cfg(test)]
+mod tests {
+    use toml;
+
+    use save::Access;
+    use save::pyramid::{Coords, Team};
+    use save::util::{ACCESS_KEY, Tomlable};
+    use super::FailureState;
+
+    #[test]
+    fn toml_round_trip() {
+        let mut state = FailureState::from_toml(toml::Value::Boolean(false));
+        state.access = Access::Replaying;
+        state.mid_scene_done = true;
+        state.board.set_piece_at(Coords::new(0, 1), Team::You);
+        state.board.set_piece_at(Coords::new(0, 5), Team::SRB);
+
+        let state = FailureState::from_toml(state.to_toml());
+        assert_eq!(state.access, Access::Replaying);
+        assert!(state.mid_scene_done);
+        assert_eq!(state.board().piece_at(Coords::new(0, 1)), Some(Team::You));
+        assert_eq!(state.board().piece_at(Coords::new(0, 5)), Some(Team::SRB));
+    }
+
+    #[test]
+    fn from_empty_toml() {
+        let state = FailureState::from_toml(toml::Value::Boolean(false));
+        assert_eq!(state.access, Access::Unvisited);
+        assert!(!state.mid_scene_is_done());
+        assert!(state.board().is_empty());
+    }
+
+    #[test]
+    fn from_solved_toml() {
+        let mut table = toml::value::Table::new();
+        table.insert(ACCESS_KEY.to_string(), Access::Solved.to_toml());
+
+        let state = FailureState::from_toml(toml::Value::Table(table));
+        assert_eq!(state.access, Access::Solved);
+        assert!(state.mid_scene_is_done());
+        assert!(!state.board().is_empty());
+        assert_eq!(state.board().you_supply(), 0);
+        assert_eq!(state.board().srb_supply(), 0);
+        assert_eq!(state.board().piece_at(Coords::new(7, 0)), Some(Team::You));
     }
 }
 

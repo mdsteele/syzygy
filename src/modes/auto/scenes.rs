cf61b52bde843c1b6aefbce7304958acb4f82e93
Matthew D. Steele <mdsteele@alum.mit.edu>
2017-09-22 19:50:40 -0400

    Add closing cutscene for "Autofac Tour" puzzle

--- src/modes/auto/scenes.rs
+++ src/modes/auto/scenes.rs
@@ -22,9 +22,12 @@ use gui::{Resources, Sound};
 
 // ========================================================================= //
 
-const ARGONY: i32 = 1;
-const MEZURE: i32 = 2;
-const YTTRIS: i32 = 3;
+const ARGONY: i32 = 2;
+const MEZURE: i32 = 3;
+const RELYNG_BG: i32 = -1;
+const RELYNG_FG: i32 = 1;
+const SYSTEM: i32 = 0;
+const YTTRIS: i32 = 4;
 
 // ========================================================================= //
 
@@ -33,6 +36,7 @@ pub fn compile_intro_scene(resources: &mut Resources) -> Scene {
     let ast = vec![
         Ast::Seq(vec![
             Ast::SetBg("autofac_tour"),
+            Ast::Place(SYSTEM, "chars/system", 0, (288, 80)),
             Ast::Wait(0.5),
             Ast::Place(MEZURE, "chars/mezure", 0, (-16, 256)),
             Ast::Slide(MEZURE, (260, 256), false, true, 1.25),
@@ -72,8 +76,17 @@ pub fn compile_intro_scene(resources: &mut Resources) -> Scene {
             Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NW,
                       "Figures."),
         ]),
+        Ast::Par(vec![
+            Ast::Loop(0, 1, Box::new(Ast::Seq(vec![
+                Ast::Slide(MEZURE, (330, 256), true, true, 1.0),
+            ]))),
+            Ast::Seq(vec![
+                Ast::Sound(Sound::talk_hi()),
+                Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NW,
+                          "Hmm..."),
+            ]),
+        ]),
         Ast::Seq(vec![
-            Ast::Slide(MEZURE, (330, 256), true, true, 1.0),
             Ast::Sound(Sound::talk_hi()),
             Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NW,
                       "What's wrong with it, anyway?\n\
@@ -175,8 +188,106 @@ pub fn compile_outro_scene(resources: &mut Resources) -> Scene {
             Ast::Sound(Sound::transform_final()),
             Ast::Wait(2.0),
             Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(YTTRIS, TalkStyle::Normal, TalkPos::NE,
+                      "Booyah!"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::beep()),
+            Ast::Talk(SYSTEM, TalkStyle::System, TalkPos::SE,
+                      "Factory automation\n\
+                       systems reengaged."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ARGONY, TalkStyle::Normal, TalkPos::NW,
+                      "Excellent."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NW,
+                      "Wait, this was supposed\n\
+                       to say ``security'' this\n\
+                       whole time?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NW,
+                      "As in, the security\n\
+                       here was disabled\n\
+                       until just now?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ARGONY, TalkStyle::Normal, TalkPos::NW,
+                      "Apparently.  Why?"),
+        ]),
+        Ast::Par(vec![
+            Ast::Seq(vec![
+                Ast::Sound(Sound::talk_hi()),
+                Ast::Talk(YTTRIS, TalkStyle::Normal, TalkPos::NE,
+                          "`Scuse me!"),
+            ]),
+            Ast::Loop(0, 1, Box::new(Ast::Seq(vec![
+                Ast::Slide(YTTRIS, (274, 256), true, false, 0.75),
+                Ast::Sound(Sound::small_jump()),
+                Ast::Jump(YTTRIS, (386, 256), 0.75),
+                Ast::Sound(Sound::small_jump()),
+                Ast::Jump(YTTRIS, (498, 256), 0.75),
+                Ast::Slide(YTTRIS, (592, 256), false, false, 0.5),
+                Ast::Remove(YTTRIS),
+            ]))),
+            Ast::Seq(vec![
+                Ast::Wait(0.5),
+                Ast::Sound(Sound::talk_hi()),
+                Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                          "Earlier, Ugrent mentioned\n\
+                           that he thought some of this\n\
+                           damage might be sabotage."),
+            ]),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
             Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
-                      "That was easy!"),
+                      "Could someone have\n\
+                       disabled the security\n\
+                       here on purpose?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ARGONY, TalkStyle::Normal, TalkPos::NW,
+                      "Hmm.  Perhaps."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ARGONY, TalkStyle::Normal, TalkPos::NW,
+                      "Fortunately, I know\n\
+                       $ijust$r  who to ask\n\
+                       about that."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Slide(ARGONY, (592, 256), true, false, 1.0),
+            Ast::Remove(ARGONY),
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "Wait, who are you\n\
+                       talking about?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Slide(MEZURE, (592, 256), true, false, 1.0),
+            Ast::Remove(MEZURE),
+            Ast::Wait(1.5),
+            Ast::Place(RELYNG_BG, "chars/relyng", 4, (344, 288)),
+            Ast::Slide(RELYNG_BG, (344, 272), false, false, 1.0),
+            Ast::Wait(1.0),
+            Ast::Sound(Sound::small_jump()),
+            Ast::SetSprite(RELYNG_BG, "chars/relyng", 0),
+            Ast::Jump(RELYNG_BG, (344, 256), 0.5),
+            Ast::Remove(RELYNG_BG),
+            Ast::Place(RELYNG_FG, "chars/relyng", 0, (344, 256)),
+            Ast::Wait(0.5),
+            Ast::Slide(RELYNG_FG, (592, 256), true, false, 1.0),
+            Ast::Remove(RELYNG_FG),
+            Ast::Wait(1.0),
         ]),
     ];
     Ast::compile_scene(resources, ast)

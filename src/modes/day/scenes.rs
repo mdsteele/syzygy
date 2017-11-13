28266870b69ab1965fb3829c94c536fcc94f78e1
Matthew D. Steele <mdsteele@alum.mit.edu>
2017-11-13 17:50:50 -0500

    Add closing cutscene for "Plane as Day" puzzle

--- src/modes/day/scenes.rs
+++ src/modes/day/scenes.rs
@@ -22,7 +22,11 @@ use gui::{Resources, Sound};
 
 // ========================================================================= //
 
+const ELINSA: i32 = 2;
 const MEZURE: i32 = 1;
+const PLATFORM: i32 = 0;
+
+const PLATFORM_INDICES: &[usize] = &[2, 3];
 
 // ========================================================================= //
 
@@ -85,7 +89,164 @@ pub fn compile_outro_scene(resources: &mut Resources) -> Scene {
         Ast::Seq(vec![
             Ast::Sound(Sound::solve_puzzle_chime()),
             Ast::Wait(1.0),
-            Ast::Slide(MEZURE, (-16, 288), true, false, 1.0),
+            Ast::Shake(2),
+            Ast::Wait(1.0),
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "Whoa.  I think we're\n\
+                       starting to move."),
+        ]),
+        Ast::Par(vec![
+            Ast::Seq(vec![
+                Ast::Place(ELINSA, "chars/elinsa", 0, (120, -48)),
+                Ast::Slide(ELINSA, (120, 112), false, false, 1.0),
+                Ast::Sound(Sound::talk_hi()),
+                Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                          "What the heck is\n\
+                           going on down here?"),
+            ]),
+            Ast::Seq(vec![
+                Ast::Sound(Sound::platform_shift(5)),
+                Ast::Place(PLATFORM, "shift/platforms", 2, (120, -16)),
+                Ast::Anim(PLATFORM, "shift/platforms", PLATFORM_INDICES, 2),
+                Ast::Slide(PLATFORM, (120, 144), false, false, 1.0),
+                Ast::SetSprite(PLATFORM, "shift/platforms", 2),
+            ]),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "I, uh, fixed the engines?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_annoyed_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "You WHAT!?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Shake(2),
+            Ast::Wait(1.0),
+            // TODO: camera cuts to space outside ship, engines start (reverse
+            // of what happens in the prolog)
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "You...you...do you\n\
+                       have any idea what\n\
+                       you've done!?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "I...got us moving again?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "Yes!  Exactly!"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "We haven't fixed the\n\
+                       navigational controls yet,\n\
+                       Mezure.  We're moving, but\n\
+                       we can't steer the ship!"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "Oh...dear.  Uh, I guess\n\
+                       we'd better pull the plug\n\
+                       on the engines, then?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_annoyed_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "We can't!  Not safely, anyway, not\n\
+                       without getting the shocks working\n\
+                       again first, $iwhich they aren't$r."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_lo()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "It's a wonder that you didn't\n\
+                       $ialready$r  blow us up by turning\n\
+                       the engines on like that, let\n\
+                       alone shutting them back $ioff$r."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "I'm sorry!\n\
+                       I didn't know!"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Slide(ELINSA, (128, 112), true, true, 0.25),
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "Okay, $ithink,$r Elinsa!\n\
+                       Maybe this isn't so bad."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "I happened to already check\n\
+                       the autopilot, so we know we're\n\
+                       still on our previous course...\n\
+                       I already fixed the gyro and the\n\
+                       attitude thrusters, so our path\n\
+                       should be stable enough..."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "So I guess we're going to end up\n\
+                       where we were already heading\n\
+                       before this disaster.  We'll just\n\
+                       have to try to finish fixing the\n\
+                       ship before we get there."),
+        ]),
+        Ast::Seq(vec![
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "Where's ``there?''  Where\n\
+                       is the ship headed?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Slide(ELINSA, (120, 112), true, true, 0.25),
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(ELINSA, TalkStyle::Normal, TalkPos::SE,
+                      "I've got to get going.\n\
+                       There's a lot to do, and\n\
+                       now we have a deadline!"),
+        ]),
+        Ast::Par(vec![
+            Ast::Seq(vec![
+                Ast::Slide(ELINSA, (120, -48), false, false, 1.0),
+                Ast::Remove(ELINSA),
+            ]),
+            Ast::Seq(vec![
+                Ast::Sound(Sound::platform_shift(5)),
+                Ast::Anim(PLATFORM, "shift/platforms", PLATFORM_INDICES, 2),
+                Ast::Slide(PLATFORM, (120, -16), false, false, 1.0),
+                Ast::Remove(PLATFORM),
+            ]),
+            Ast::Seq(vec![
+                Ast::Wait(0.5),
+                Ast::Sound(Sound::talk_hi()),
+                Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                          "W- Wait!"),
+            ]),
+        ]),
+        Ast::Seq(vec![
+            Ast::Wait(1.0),
+            Ast::Sound(Sound::talk_hi()),
+            Ast::Talk(MEZURE, TalkStyle::Normal, TalkPos::NE,
+                      "Why does no one ever\n\
+                       $itell$r  me anything?"),
+        ]),
+        Ast::Seq(vec![
+            Ast::Slide(MEZURE, (-16, 288), true, false, 0.75),
             Ast::Remove(MEZURE),
             Ast::Wait(1.0),
             Ast::Queue(0, 1),
